254e14e353b23f4149b726cb07b4ca652ebf966a
snayan <snayan@sina.com>
2016-05-14 12:20:11 +0800

    compete customer-event on off

--- demo.js
+++ demo.js
@@ -1,19 +1,19 @@
-var obj=new Event();
-obj.on("click",function(s){
-  //do something in callback
-});
-obj.on("dbclick",function(s){
-  //do something in callback
-});
-console.log(obj._events);
-//print
-{
-  "click":[{
-    callback:function,
-    context:this
-  }],
-  "dbclick":[{
-    callback:function,
-    context:this
-  }]
+// console.log(MyEvent);
+function click(msg){
+  alert("click event msg is " + msg);
 }
+MyEvent.on("click",click);
+MyEvent.on("click",click,MyEvent);
+MyEvent.on("click",click,this);
+MyEvent.on("change",function(msg){
+  alert("change event msg is "+ msg);
+});
+MyEvent.on("click dbclick",function(msg){
+  alert("click dbclick event msg is " + msg);
+},this);
+// MyEvent.off("click");
+// MyEvent.off("click",click);
+// MyEvent.off("click",null,MyEvent);
+MyEvent.off("click",click,MyEvent);
+
+console.log(MyEvent);
--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 (function(self){
 
-  var Event=self.Event||{};
+  var MyEvent=self.MyEvent||(self.MyEvent={});
 
   var _eventSplit=/\s+/;
 
@@ -9,8 +9,8 @@
     if(name){
       var handlers=events[name]||(events[name]=[]);
       handlers.push({
-        callback:callback||void 0,
-        context:context
+        callback:callback||function(){},
+        context:context||null
       });
     }
     return events;
@@ -21,61 +21,75 @@
     if(!events){return;}
     names=name?[name]:_keys(events);
     var handlers=[],remain=[];
-    names.forEach(function(i,n){
+    _forEach(names,function(n){
       handlers=events[n],remain=[];
-      handlers.forEach(function(j,d){
-        if((callback&&callback!==d.callback)||(context&&context!==d.context)){
-          remain.push(d);
+      _forEach(handlers,function(h){
+        if((callback&& callback!==h.callback)||(context&&context!==h.context)){
+          remain.push(h);
         }
       });
       if(remain.length>0){
-        events[n]=remain;
-      }
+          events[n]=remain;
+        }
+        else{
+          delete events[n];
+        }
     });
+    return events;
   }
 
   //事件API迭代
   function _eventApi(iterate,events,name,callback,context){
     if(typeof name ==="string"){
       var names=_eventSplit.test(name)?name.split(_eventSplit):[name];
-      names.forEach(function(i,n){
+      _forEach(names,function(n){
         events=iterate(events,n,callback,context);
       });
     }
-    reutrn events;
+    return events;
   }
 
   //将对象的属性以数组形式返回
   function _keys(object){
     var arr=[];
     if(object && typeof object==="object"){
-      forEach(var a in object){
+      for(var a in object){
         arr.push(a);
       }
     }
     return arr;
   }
 
+  //遍历数组
+  function _forEach(array,callback){
+    if(array && callback && array instanceof Array && callback instanceof Function){
+      for(var i=0,j=array.length;i<j;i++){
+        callback.call(null,array[i]);
+      }
+    }
+  }
   //添加事件
-  Event.on=function(name,callback,context){
+  MyEvent.on=function(name,callback,context){
     this._events=_eventApi(_addEvent,this._events||(this._events={}),name,callback,context);
     return this;
   },
 
   //移除事件
-  Event.off=function(name,callback,context){
+  MyEvent.off=function(name,callback,context){
     this._events=_eventApi(_offEvent,this._events||(this._events={}),name,callback,context);
     return this;
   },
 
   //触发事件
-  Event.trigger=function(name){
+  MyEvent.trigger=function(name){
 
   },
 
   //添加只执行一次的事件
-  Event.once=function(name,callback,context){
+  MyEvent.once=function(name,callback,context){
 
   }
+
+  // console.log(MyEvent);
 
 })(window);
