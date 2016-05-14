ab498e7dfa329705b3584e8a685fb6ae2202bfb5
snayan <snayan@sina.com>
2016-05-14 16:05:32 +0800

    complete once and trigger

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 // console.log(MyEvent);
-function click(msg){
-  alert("click event msg is " + msg);
+function click(msg,msg1){
+  alert("click event msg is " + msg +" and msg1 is " + msg1 );
 }
 MyEvent.on("click",click);
 MyEvent.on("click",click,MyEvent);
@@ -12,8 +12,14 @@ MyEvent.on("click dbclick",function(msg){
   alert("click dbclick event msg is " + msg);
 },this);
 // MyEvent.off("click");
-// MyEvent.off("click",click);
+ // MyEvent.off("click",click);
 // MyEvent.off("click",null,MyEvent);
-MyEvent.off("click",click,MyEvent);
+// MyEvent.off();
 
+// MyEvent.trigger("click","msg","msg1");
+MyEvent.once("aa",function(msg){
+  alert("once event test params is " + msg );
+});
+MyEvent.trigger("aa","once");
+MyEvent.trigger("aa","once");
 console.log(MyEvent);
--- events.js
+++ events.js
@@ -5,12 +5,14 @@
   var _eventSplit=/\s+/;
 
   //将指定事件添加到事件集合中
-  function _addEvent(events,name,callback,context){
+  function _addEvent(events,name,callback,context,opts){
     if(name){
       var handlers=events[name]||(events[name]=[]);
+      opts=opts||{};
       handlers.push({
         callback:callback||function(){},
-        context:context||null
+        context:context||null,
+        once:opts.once||false
       });
     }
     return events;
@@ -38,11 +40,41 @@
     return events;
   }
 
+  //触发事件
+  function _triggerEvent(events,name,callback,context,opts){
+    if(!events)return;
+    var handlers=events[name]||[];
+    var args=opts.arguments||[];
+    _forEach(handlers,function(h){
+      _internalCall(h.callback,h.context,args);
+      //如果是只触发一次的事件，则触发之后移除
+      if(h.once){
+        events=_offEvent(events,name,callback,context);
+      }
+    });
+    return events;
+  }
+
+  //内部出发事件
+  function _internalCall(callback,context,args){
+    if(callback && callback instanceof Function){
+      args=args.slice();
+      var l=args.length;
+      switch(l){
+        case 0:callback.call(context);return;
+        case 1:callback.call(context,args[0]);return;
+        case 2:callback.call(context,args[0],args[1]);return;
+        case 3:callback.call(context,args[0],args[1],args[2]);return;
+        default:callback.apply(context,args);return;
+      }
+    }
+  }
+
   //事件API迭代
-  function _eventApi(iterate,events,name,callback,context){
+  function _eventApi(iterate,events,name,callback,context,opts){
     var names=_eventSplit.test(name)?name.split(_eventSplit):[name];
       _forEach(names,function(n){
-        events=iterate(events,n,callback,context);
+        events=iterate(events,n,callback,context,opts);
       });
     return events;
   }
@@ -67,8 +99,8 @@
     }
   }
   //添加事件
-  MyEvent.on=function(name,callback,context){
-    this._events=_eventApi(_addEvent,this._events||(this._events={}),name,callback,context);
+  MyEvent.on=function(name,callback,context,opts){
+    this._events=_eventApi(_addEvent,this._events||(this._events={}),name,callback,context,opts);
     return this;
   },
 
@@ -80,14 +112,27 @@
 
   //触发事件
   MyEvent.trigger=function(name){
+    if(!this._events)return;
+
+    //获取到去除name的参数，并把这些参数传递给callback
+    var l=Math.max(0,arguments.length-1);
+    var args=new Array(l);
+    for(var i=0;i<l;i++){
+      args[i]=arguments[i+1];
+    }
+
+    this._events=_eventApi(_triggerEvent,this._events,name,null ,null,{arguments:args});
+
+    return this;
 
   },
 
   //添加只执行一次的事件
   MyEvent.once=function(name,callback,context){
 
+    this.on(name,callback,context,{once:true});
+
   }
 
-  // console.log(MyEvent);
 
 })(window);
